a1c476db1569ad8751e7a53c7787dc1b7e350ea9
Tiya Lee <Bigtiyalee@gmail.com>
2021-07-29 10:35:48 -0400

    connected mongodc

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
-require('mongodb');
-require('mongoose');
-
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
 
 let Person;
 
